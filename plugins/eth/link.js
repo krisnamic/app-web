ec6d8665211f2575942499d1526b0e3686d24a56
Michael Krisna Cahyadi <cahyadi.michael.mkc24@gmail.com>
2022-05-05 09:13:23 +0700

    add rinkeby, polygon, bsc network

--- plugins/eth/link.js
+++ plugins/eth/link.js
@@ -1,13 +1,13 @@
-const EXPLORER_DOMAIN = 'etherscan.io'
-
 export default class Link {
   address (addr, { network = 'mainnet' } = {}) {
     const subdomain = network === 'mainnet' ? '' : `${network}.`
+    const EXPLORER_DOMAIN = network === 'bsc' || network === 'bsctest' ? 'bscscan.com' : (network === 'polygon' || network === 'mumbai' ? 'polygonscan.com' : 'etherscan.io')
     return `https://${subdomain}${EXPLORER_DOMAIN}/address/${addr}`
   }
 
   transaction (txHash, { network = 'mainnet' } = {}) {
     const subdomain = network === 'mainnet' ? '' : `${network}.`
+    const EXPLORER_DOMAIN = network === 'bsc' || network === 'bsctest' ? 'bscscan.com' : (network === 'polygon' || network === 'mumbai' ? 'polygonscan.com' : 'etherscan.io')
     return `https://${subdomain}${EXPLORER_DOMAIN}/tx/${txHash}`
   }
 }
